403d16766599b48aaafaa1ce0e6c38971b65287d
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-19 20:12:38 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,11 @@
 // your Bomb code here!
+// your Bomb code here!
+import React, { Component } from 'react';
+class Bomb extends Component {
+    state = {  }
+    render() {
+        return (  );
+    }
+}
+
+export default Bombs
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,11 @@
 // your ImageSlider code here!
+// your ImageSlider code here!
+import React, { Component } from 'react';
+class ImageSlider extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default ImageSlider;
